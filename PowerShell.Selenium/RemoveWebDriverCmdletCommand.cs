68ad5279215406863dbd5bf3e2071ab834bdfcab
Vivek Saurabh <saurabhvivek30@gmail.com>
2024-04-03 19:59:22 +0530

    added remove driver

---
+++ PowerShell.Selenium/RemoveWebDriverCmdletCommand.cs
@@ -0,0 +1,31 @@
+using System.Management.Automation;
+using OpenQA.Selenium;
+
+namespace PowerShell.Selenium
+{
+    [Cmdlet(VerbsCommon.Remove, "WebDriver")]
+    [OutputType(typeof(void))]
+    public class RemoveWebDriverCmdletCommand : PSCmdlet
+    {
+        [Parameter(
+            Mandatory = true,
+            Position = 0)]
+        public IWebDriver Driver { get; set; }
+
+        protected override void BeginProcessing()
+        {
+            base.BeginProcessing();
+        }
+
+        protected override void ProcessRecord()
+        {
+            if(Driver != null)
+                Driver.Quit();
+        }
+
+        protected override void EndProcessing()
+        {
+            base.EndProcessing();
+        }
+    }
+}
